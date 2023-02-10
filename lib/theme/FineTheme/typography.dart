2a58412c9e97053d866c881b72ba4e86fbf8f2ff
Smjle <denwingsa01884@gmail.com>
2023-02-11 00:10:04 +0700

    fine theme

--- lib/theme/FineTheme/typography.dart
+++ lib/theme/FineTheme/typography.dart
@@ -11,4 +11,54 @@ class FineTypograhpy {
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w600,
       fontSize: 20);
+  TextStyle buttonLg = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w700,
+      fontSize: 16);
+  TextStyle buttonMd = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+      fontSize: 14);
+  TextStyle buttonSm = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+      fontSize: 12);
+  TextStyle subtitle1 = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+      fontSize: 16);
+  TextStyle subtitle2 = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+      fontSize: 14);
+  TextStyle body1 = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+      fontSize: 16);
+  TextStyle body2 = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+      fontSize: 14);
+  TextStyle caption1 = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+      fontSize: 12);
+  TextStyle caption2 = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+      fontSize: 12);
+  TextStyle overline = TextStyle(
+      fontStyle: FontStyle.normal,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+      fontSize: 10);
 }
